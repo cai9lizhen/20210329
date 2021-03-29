3fd2e56d201c433d3d8530cd19d23db36f413380
student <o1083008@gm.pu.edu.tw>
2021-03-29 15:18:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a20210329/MainActivity.java
@@ -0,0 +1,9 @@
+package com.example.a20210329;
+
+import android.app.Activity;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    public void add(View view) {
+    }
+}
---
+++ app/src/main/java/com/example/a20210329/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.jupiter.application.counter2_030222021
+
+import android.os.Bundle
+import android.os.Vibrator
+import android.view.MotionEvent
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnLongClickListener, View.OnTouchListener {
+
+    var counter : Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        txv.setOnClickListener(this)
+        img.setOnClickListener(this)
+        btn4.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(v: View?) {
+                counter = (1..100).random()
+                txv.text = counter.toString()
+            }
+        })
+
+        txv.setOnLongClickListener(object:View.OnLongClickListener{
+            override fun onLongClick(v: View?): Boolean {
+                counter+=2
+                txv.text = counter.toString()
+                return true
+            }
+        })
+
+        txv2.setOnTouchListener(object :View.OnTouchListener{
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                if(event?.action == MotionEvent.ACTION_DOWN){
+                    txv2.text="TEST OK_DOWN(Press)"
+                }
+                else if(event?.action == MotionEvent.ACTION_UP){
+                    txv2.text="TEST OK_UP(Release)"
+                }
+                return true
+            }
+        })
+
+    }
+
+
+
+    fun add(v:View){
+        if(v == btn1){
+            counter++
+        }
+        else if(v == btn2){
+            counter+=2
+        }
+        else if(v == btn3){
+            counter = 0
+        }
+        txv.text = counter.toString()
+
+    }
+
+    override fun onClick(v: View?) {
+        if(v == txv){
+            counter++
+        }
+        else if(v == img){
+            counter+=2
+        }
+        /* else if(v == btn4){
+            counter = (0..100).random()
+            txv.text = counter.toString()
+
+        }*/
+        txv.text = counter.toString()
+    }
+
+    override fun onLongClick(v: View?): Boolean {
+        counter+=2
+        txv.text = counter.toString()
+        return true
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+
+}
